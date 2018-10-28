099e0dcf2d7add318f4b148d6f5ce00d040a2784
shizhuobin <853124420@qq.com>
2018-10-28 13:31:51 +0800

    ss

---
+++ src/wangzherongyao/ZhanChangTest.java
@@ -0,0 +1,31 @@
+package wangzherongyao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ZhanChangTest {
+
+	@Test
+	void testSet() {
+		ZhanChang map=new ZhanChang();
+		map.set();
+		
+	}
+
+	@Test
+	void testChuLiZhiLing() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testQingLiZhangAi() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testShow() {
+		fail("Not yet implemented");
+	}
+
+}
